dc1f69810b6cdb4a91f9fb931ddbb7ad16fd3ebf
alex2772 <alex2772sc@gmail.com>
2026-05-09 09:22:26 +0300

    DIARY_INJECTION_MAX_LENGTH disable (superseded by queryAI)

--- src/config.h
+++ src/config.h
@@ -473,7 +473,7 @@ world) in the following format:
 
     static constexpr auto DIARY_TOKEN_COUNT_TRIGGER = 20000;
     static constexpr auto DIARY_AVERAGE_ENTRY_SIZE = 1000;
-    static constexpr auto DIARY_INJECTION_MAX_LENGTH = DIARY_AVERAGE_ENTRY_SIZE;
+    static constexpr auto DIARY_INJECTION_MAX_LENGTH = DIARY_AVERAGE_ENTRY_SIZE * 0; // superseded by queryAI
     static constexpr auto DIARY_SLEEP_MAX_LENGTH = DIARY_AVERAGE_ENTRY_SIZE * 40;
     static constexpr auto DIARY_PLAGIARISM_THRESHOLD = 0.97f;
     static constexpr auto DIARY_MIN_RELATEDNESS = 0.8f;
